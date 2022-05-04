2ecdfe0fd18f7daedabd90f75df6b0b01af6f4b1
Jaemin Kim <huple.jaemin@gmail.com>
2022-05-05 08:54:09 +0900

    Initial Commit

---
+++ 8degrees/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  8degrees
+//
+//  Created by 김재민 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 8degrees/_degreesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _degreesApp.swift
+//  8degrees
+//
+//  Created by 김재민 on 2022/05/05.
+//
+
+import SwiftUI
+
+@main
+struct _degreesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
