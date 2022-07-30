4be4d5e4e3b5f4db73f28aed1739b47ecae7663e
jkimdev <jkim.dev@icloud.com>
2022-07-30 21:34:15 +0900

    change fonts

--- 8degrees/Extensions&Controller/FontExtension.swift
+++ 8degrees/Extensions&Controller/FontExtension.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 extension Font {
     static func fontRegular(size: Int) -> Font {
-        return Font.custom("ONEMobileOTFRegular", size: CGFloat(size))
+        return Font.custom("DalseoDarling", size: CGFloat(size))
     }
     
     static func fontLight(size: Int) -> Font {
-        return Font.custom("ONEMobileOTFLight", size: CGFloat(size))
+        return Font.custom("DalseoHealingBold", size: CGFloat(size))
     }
     
     static func fontBold(size: Int) -> Font {
-        return Font.custom("ONEMobileOTFBold", size: CGFloat(size))
+        return Font.custom("DalseoHealingBold", size: CGFloat(size))
     }
 }
+    
