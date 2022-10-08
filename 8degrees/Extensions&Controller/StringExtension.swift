5b47d39b5842f5d99afb1fb5c9ca08ce811330ab
jkimdev <jkim.dev@icloud.com>
2022-10-08 20:10:28 +0900

    create D-day extension

---
+++ 8degrees/Extensions&Controller/StringExtension.swift
@@ -0,0 +1,21 @@
+//
+//  StringExtension.swift
+//  8degrees
+//
+//  Created by 김재민 on 2022/10/07.
+//
+
+import Foundation
+
+extension String {
+    func dayUntil(_ currentDate: Date) -> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let currentDate = Date.now
+        let targetDate = dateFormatter.date(from: self)!
+        let day = Calendar.current.dateComponents([.day],
+                                                  from: currentDate,
+                                                  to: targetDate).day ?? 0
+        return day != 0 ? String(day) : "Day"
+    }
+}
