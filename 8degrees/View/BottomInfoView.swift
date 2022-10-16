859124a833dce175ec9f4f0de0b7e13ef9afbc90
jkimdev <jkim.dev@icloud.com>
2022-10-16 12:40:21 +0900

    Genre code to string in Bottom info view

--- 8degrees/View/BottomInfoView.swift
+++ 8degrees/View/BottomInfoView.swift
@@ -17,7 +17,7 @@ struct BottomInfoView: View {
                 .frame(width: 115, height: 150, alignment: .center)
                 .cornerRadius(8)
             VStack(alignment: .leading, spacing: 8) {
-                Text(performance.genre)
+                Text(genreToString(performance.genre))
                     .font(.fontLight(size: 8))
                 Text(performance.title)
                     .font(.fontBold(size: 16))
@@ -28,10 +28,31 @@ struct BottomInfoView: View {
         }
         .padding(.horizontal)
     }
+    
+    func genreToString(_ genre: String) -> String {
+        switch genre {
+        case "AAAA":
+            return "연극"
+        case "AAAB":
+            return "뮤지컬"
+        case "BBBA":
+            return "무용"
+        case "CCCA":
+            return "클래식"
+        case "CCCB":
+            return "오페라"
+        case "CCCC":
+            return "국악"
+        case "EEEA":
+            return "복합"
+        default:
+            return "UNKNOWN"
+        }
+    }
 }
 
 struct BottomInfoView_Previews: PreviewProvider {
     static var previews: some View {
-        BottomInfoView(performance: Performance(performanceId: "P001", title: "재밌는 공연", poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF163755_200422_092850.gif", story: "This is a story", genre: "Musical", rating: "전체", startDate: "2022-09-01", endDate: "2022-09-21", state: "ONGOING", runtime: "2시간"))
+        BottomInfoView(performance: Performance(performanceId: "P001", title: "재밌는 공연", poster: "http://www.kopis.or.kr/upload/pfmPoster/PF_PF163755_200422_092850.gif", story: "This is a story", genre: "AAAA", rating: "전체", startDate: "2022-09-01", endDate: "2022-09-21", state: "ONGOING", runtime: "2시간"))
     }
 }
