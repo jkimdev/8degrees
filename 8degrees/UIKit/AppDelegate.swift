09f1467464d40d31c2a836c34570a78a4c5df937
jkimdev <jkim.dev@icloud.com>
2023-01-16 10:06:08 +0900

    splash screen sleep 2seconds

--- 8degrees/UIKit/AppDelegate.swift
+++ 8degrees/UIKit/AppDelegate.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Thread.sleep(forTimeInterval: 2.0)
         UINavigationBar.appearance().backIndicatorImage = UIImage(systemName: "arrow.left.circle.fill")?.withTintColor(.white, renderingMode: .alwaysOriginal)
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(systemName: "arrow.left.circle.fill")?.withTintColor(.white, renderingMode: .alwaysOriginal)
-        UINavigationBar.appearance().tintColor = .white
-        UINavigationBar.appearance().barTintColor = .clear
+//        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().barTintColor = UIColor(.clear)
+        
         return true
     }
 }
